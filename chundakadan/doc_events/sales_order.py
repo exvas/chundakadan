d829be8232fb6ca58e05a8f7245d711db36378df
Vijila Vk <vijilavk@Vijilas-MacBook-Air.local>
2025-08-26 12:26:18 +0530

    feat:add validation in sales order

---
+++ chundakadan/doc_events/sales_order.py
@@ -0,0 +1,15 @@
+import frappe
+
+
+@frappe.whitelist()
+def validate_item_qty_in_stock(doc, method=None):
+    for item in doc.items:
+        if item.item_code and item.warehouse:
+                available_qty=frappe.get_value("Bin",
+                {"item_code":item.item_code,"warehouse":item.warehouse
+                },"actual_qty")
+                if available_qty<item.qty:
+                        frappe.throw(
+                        f"Item {item.item_code} in warehouse {item.warehouse}: "
+                        f"Only {available_qty} available, but {item.qty} required."
+                    )
