48c2da7a969d8952337c340b5be154026aa34222
Vijila Vk <vijilavk@Vijilas-MacBook-Air.local>
2025-08-26 16:24:10 +0530

    fix:add special order check box in sales order

--- chundakadan/doc_events/sales_order.py
+++ chundakadan/doc_events/sales_order.py
@@ -3,14 +3,15 @@ import frappe
 
 @frappe.whitelist()
 def validate_item_qty_in_stock(doc, method=None):
-    for item in doc.items:
-        if item.item_code and item.warehouse:
-                available_qty=frappe.get_value("Bin",
-                {"item_code":item.item_code,"warehouse":item.warehouse
-                },"actual_qty")
-                if available_qty<item.qty:
-                        frappe.throw(
-                        f"Item {item.item_code} in warehouse {item.warehouse}: "
-                        f"Only {available_qty} available, but {item.qty} required.",
-                        title="Insufficient Stock"
-                    )
+    if not doc.custom_special_order:
+        for item in doc.items:
+            if item.item_code and item.warehouse:
+                    available_qty=frappe.get_value("Bin",
+                    {"item_code":item.item_code,"warehouse":item.warehouse
+                    },"actual_qty")
+                    if available_qty<item.qty:
+                            frappe.throw(
+                            f"Item {item.item_code} in warehouse {item.warehouse}: "
+                            f"Only {available_qty} available, but {item.qty} required.",
+                            title="Insufficient Stock"
+                        )
