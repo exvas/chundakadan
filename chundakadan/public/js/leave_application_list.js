3ee00b2fc129b8c2192b6becadc0f65c65562d60
Niranjana C <niranjanac@Niranjanas-MacBook-Air.local>
2026-02-02 14:15:21 +0530

    fix: standardized multi-click approval and fixed list view indicator

--- chundakadan/public/js/leave_application_list.js
+++ chundakadan/public/js/leave_application_list.js
@@ -13,19 +13,12 @@ frappe.listview_settings['Leave Application'].formatters = Object.assign({}, ori
     custom_approval_status: function (value, df, doc) {
         if (!value) return '';
 
-        const current_user = frappe.session.user;
         const hrms_status = doc.status;  // HRMS standard status field
-        let display_status = value;
         let indicator_color = 'blue';
 
-        // Contextual display: When status is "Approved HR", show "Approved GM" to non-GM users
-        // GM sees actual "Approved HR" so they can click approve
-        if (value === "Approved HR" && current_user !== GM_EMAIL && hrms_status !== "Approved") {
-            display_status = "Approved GM";
-            indicator_color = 'green';
-        }
         // Determine indicator color based on status
-        else if (hrms_status === "Approved") {
+        // Everyone sees the actual stored status value
+        if (hrms_status === "Approved") {
             indicator_color = 'green';
         } else if (value.startsWith("Approved")) {
             indicator_color = 'blue';
@@ -35,6 +28,6 @@ frappe.listview_settings['Leave Application'].formatters = Object.assign({}, ori
             indicator_color = 'orange';
         }
 
-        return `<span class="indicator-pill ${indicator_color}">${display_status}</span>`;
+        return `<span class="indicator-pill ${indicator_color}">${__(value)}</span>`;
     }
 });
