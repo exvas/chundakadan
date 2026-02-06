7592436efb2f13ede4941809ca2cda3946dae3e5
Niranjana C <niranjanac@Niranjanas-MacBook-Air.local>
2026-02-06 14:04:51 +0530

    leave balance report override

--- chundakadan/__init__.py
+++ chundakadan/__init__.py
@@ -1,1 +1,12 @@
 __version__ = "0.0.1"
+
+import frappe
+
+# Monkey patch standard report with custom implementation
+try:
+	import hrms.hr.report.employee_leave_balance.employee_leave_balance as standard_report
+	from chundakadan.overrides import employee_leave_balance as custom_report
+	
+	standard_report.execute = custom_report.execute
+except ImportError:
+	pass
