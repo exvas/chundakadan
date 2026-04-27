026a8f543a54a25448ab0de2b5f452362f0bc136
nahala11 <130729211+nahala11@users.noreply.github.com>
2026-04-27 16:19:25 +0530

    data import issue

--- chundakadan/chundakadan/doctype/finish_item/finish_item.py
+++ chundakadan/chundakadan/doctype/finish_item/finish_item.py
@@ -1,9 +1,28 @@
-# Copyright (c) 2025, Ashkar and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
-
 class FinishItem(Document):
-	pass
+	def validate(self):
+		self.validate_unique_finish_per_group()
+
+	def validate_unique_finish_per_group(self):
+		if not self.finish or not self.item_group:
+			return
+
+		# Check if another record with the same finish and item_group exists
+		duplicate = frappe.db.exists(
+			"Finish Item",
+			{
+				"finish": self.finish,
+				"item_group": self.item_group,
+				"name": ["!=", self.name]
+			}
+		)
+
+		if duplicate:
+			frappe.throw(
+				_("Finish '{0}' already exists for Item Group '{1}'").format(
+					self.finish, self.item_group
+				)
+			)
