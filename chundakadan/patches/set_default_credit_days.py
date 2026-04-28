53903174f18732dc74ffab4f98a176e7e48c2065
Niranjana C <niranjanac@Niranjanas-MacBook-Air.local>
2026-04-28 15:53:08 +0530

    patch column fix in db

--- chundakadan/patches/set_default_credit_days.py
+++ chundakadan/patches/set_default_credit_days.py
@@ -1,4 +1,7 @@
 import frappe
 
 def execute():
+	if not frappe.db.has_column("Customer", "custom_credit_days"):
+		frappe.db.sql("ALTER TABLE `tabCustomer` ADD COLUMN `custom_credit_days` varchar(255)")
+		
 	frappe.db.sql("UPDATE `tabCustomer` SET custom_credit_days = 45")
