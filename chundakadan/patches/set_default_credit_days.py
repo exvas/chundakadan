992d4989bfc233c1228ea3f87cf4380b5a628060
Niranjana C <niranjanac@Niranjanas-MacBook-Air.local>
2026-04-30 15:01:13 +0530

    chore: remove custom_credit_days field and patch

--- chundakadan/patches/set_default_credit_days.py
+++
@@ -1,7 +0,0 @@
-import frappe
-
-def execute():
-	if not frappe.db.has_column("Customer", "custom_credit_days"):
-		frappe.db.sql("ALTER TABLE `tabCustomer` ADD COLUMN `custom_credit_days` varchar(255)")
-		
-	frappe.db.sql("UPDATE `tabCustomer` SET custom_credit_days = 45")
