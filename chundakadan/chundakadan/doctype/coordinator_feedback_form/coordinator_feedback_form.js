46ad8841caddac9e92484e37c1e97fef551eef70
Niranjana C <niranjanac@Niranjanas-MacBook-Air.local>
2026-05-11 17:57:51 +0530

    3 new docs

---
+++ chundakadan/chundakadan/doctype/coordinator_feedback_form/coordinator_feedback_form.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Ashkar and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Coordinator Feedback Form", {
+// 	refresh(frm) {
+
+// 	},
+// });
