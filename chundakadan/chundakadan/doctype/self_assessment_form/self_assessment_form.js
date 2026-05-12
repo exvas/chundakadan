b5ff6b8c8ed2a04ac60ff8ef38f4d9c68b8512ce
Niranjana C <niranjanac@Niranjanas-MacBook-Air.local>
2026-05-12 12:43:31 +0530

    feedback forms json setup

--- chundakadan/chundakadan/doctype/self_assessment_form/self_assessment_form.js
+++ chundakadan/chundakadan/doctype/self_assessment_form/self_assessment_form.js
@@ -4,7 +4,6 @@
 frappe.ui.form.on("Self Assessment Form", {
 	refresh(frm) {
 		frm.trigger('render_instructions');
-		frm.trigger('render_rating_table');
 	},
 	render_instructions(frm) {
 		const instructions_html = `
@@ -21,73 +20,15 @@ frappe.ui.form.on("Self Assessment Form", {
 			$(frm.fields_dict.custom_instructions_keep_this_same_everywhere.wrapper).html(instructions_html);
 		}
 	},
-	render_rating_table(frm) {
-		const questions = [
-			{ field: 'meet_targets', label: 'I consistently meet my targets' },
-			{ field: 'dealer_relationships', label: 'I maintain strong dealer relationships' },
-			{ field: 'processes_reporting', label: 'I follow company processes and reporting' },
-			{ field: 'handle_challenges', label: 'I handle challenges effectively' },
-			{ field: 'contribute_team', label: 'I contribute to team success' }
-		];
-
-		let html = `
-			<div style="font-family: 'Times New Roman', serif; color: #000; padding: 20px; background: #fff;">
-				<h4 style="font-weight: bold; margin-bottom: 25px; font-size: 18px;">Rate the following:</h4>
-				<table style="width: 100%; border-collapse: collapse; margin-bottom: 30px;">
-					<tbody>
-		`;
-
-		questions.forEach((q, index) => {
-			const current_val = frm.doc[q.field] || '';
-			html += `
-				<tr style="height: 50px; vertical-align: bottom;">
-					<td style="width: 40px; font-size: 17px; padding-bottom: 10px;">${index + 1}.</td>
-					<td style="font-size: 17px; padding-bottom: 10px;">${q.label}</td>
-					<td style="width: 180px; text-align: right; padding-bottom: 10px;">
-						<input type="number" min="1" max="5" value="${current_val}" 
-							style="width: 60px; border: none; border-bottom: 1px solid #000; text-align: center; font-size: 17px; outline: none; background: transparent;"
-							onchange="cur_frm.trigger('update_rating', '${q.field}', this.value)">
-						<span style="font-size: 17px; margin-left: 5px;">/ 5</span>
-					</td>
-				</tr>
-			`;
-		});
-
-		const overall = frm.doc.overall_self_rating || '';
-		html += `
-					</tbody>
-				</table>
-				<div style="margin-top: 40px; font-size: 20px; font-weight: bold;">
-					Overall Self Rating: 
-					<input type="number" min="1" max="5" value="${overall}" 
-						style="width: 80px; border: none; border-bottom: 2px solid #000; text-align: center; font-size: 20px; font-weight: bold; outline: none; background: transparent;"
-						onchange="cur_frm.trigger('update_rating', 'overall_self_rating', this.value)">
-					<span style="font-size: 20px; margin-left: 5px;">/ 5</span>
-				</div>
-			</div>
-		`;
-
-		if (frm.fields_dict.rating_html) {
-			$(frm.fields_dict.rating_html.wrapper).html(html);
-		}
-	},
-	update_rating(frm, field, value) {
-		let val = parseInt(value);
-		if (isNaN(val) || val < 1) val = 0;
-		if (val > 5) val = 5;
-		
-		frm.set_value(field, val);
-	},
 	custom_executive_name(frm) {
 		if (frm.doc.custom_executive_name) {
-			frappe.db.get_doc("Employee", frm.doc.custom_executive_name)
-				.then((doc) => {
-					frm.set_df_property("custom_executive_name", "description", `Employee Name: <b>${doc.employee_name}</b>`);
-					frm.set_value("custom_employee_code", doc.name);
-					frm.set_value("custom_departmentterritory", doc.department);
-				});
+			frappe.db.get_value("Employee", frm.doc.custom_executive_name, ["name", "department", "employee_name"], (r) => {
+				if (r) {
+					frm.set_value("custom_employee_code", r.name);
+					frm.set_value("custom_departmentterritory", r.department);
+				}
+			});
 		} else {
-			frm.set_df_property("custom_executive_name", "description", "");
 			frm.set_value("custom_employee_code", "");
 			frm.set_value("custom_departmentterritory", "");
 		}
